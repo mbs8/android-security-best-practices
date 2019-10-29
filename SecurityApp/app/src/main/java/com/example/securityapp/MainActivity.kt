62b45f746c612d5562a6873ccd1b50ad73069cdf
Branco <mbs8@cin.ufpe.br>
2019-10-29 19:07:16 -0300

    Organiza os botões na MainActivity e cria as telas do mapa e da câmera

--- SecurityApp/app/src/main/java/com/example/securityapp/MainActivity.kt
+++ SecurityApp/app/src/main/java/com/example/securityapp/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.securityapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapButton.setOnClickListener{
+            val mapIntent = Intent(applicationContext, MapActivity::class.java)
+            startActivity(mapIntent)
+        }
+        cameraButton.setOnClickListener{
+            val cameraIntent = Intent(applicationContext, CameraActivity::class.java)
+            startActivity(cameraIntent)
+        }
     }
 }
